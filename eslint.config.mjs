f82f1c173940cc9c9d51302cb5fff0be6da76968
MoonDev <mriveroom@gmail.com>
2025-03-04 04:03:24 +0900

    chore: ESLint 구성 업데이트 및 Prettier 플러그인 통합

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,12 +11,10 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
-  ...compat.plugins('prettier'),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  eslintPluginPrettierRecommended,
   {
     rules: {
-      semi: ['error', 'always'], // 세미콜론 필수
-      indent: ['error', 2], // 들여쓰기 2칸
       'no-console': 'warn', // console.log 사용 시 경고
       'no-unused-vars': 'warn', // 사용되지 않는 변수 경고
       'react/jsx-key': 'error', // JSX에서 key 필수
